14941b06fc5963c5354d8c6fad897750cc7c3d60
JooJo <sjak0101@gmail.com>
2022-06-03 02:08:44 +0100

    first commit of clean funcational baper bank

---
+++ js/banking.js
@@ -0,0 +1,54 @@
+// hande deposit button event
+document.getElementById('deposit-button').addEventListener('click', function(){
+    // get the amount deposited
+    const depositInput = document.getElementById('deposit-input');
+    const newDepositText = depositInput.value;
+    const newDepositAmount = parseFloat(newDepositText);
+    //console.log(depositAmount);
+
+    // Update Deposit total
+    const depositTotal = document.getElementById('deposit-total');
+    const previousDepositText = depositTotal.innerText;
+    const previousDepositAmount = parseFloat(previousDepositText);
+    const newDepositTotal = previousDepositAmount + newDepositAmount;
+
+    depositTotal.innerText = newDepositTotal;
+
+    // update account balance
+    const balanceTotal = document.getElementById('balance-total');
+    const balanceTotalText = balanceTotal.innerText;
+    const previousBalanceTotal = parseFloat(balanceTotalText);
+    const newBalanceTotal = previousBalanceTotal + newDepositAmount;
+    balanceTotal.innerText = newBalanceTotal;
+    // clear the deposit input field
+    depositInput.value = ' ';
+});
+
+// hande withdraw evenhandler
+
+document.getElementById('withdraw-button').addEventListener('click', function(){
+    // get the withdraw ammount
+    const withdrawInput = document.getElementById('withdraw-input');
+    const newWithdrawText = withdrawInput.value;
+    const newWithdrawAmount = parseFloat(newWithdrawText);
+
+    // set withdraw total
+
+    const withdrawTotal = document.getElementById('withdraw-total');
+    const previousWithdrawText = withdrawTotal.innerText;
+    const previousWithdrawTotal = parseFloat(previousWithdrawText);
+    const newWithdrawTotal = previousWithdrawTotal + newWithdrawAmount;
+
+    withdrawTotal.innerText = newWithdrawTotal;
+
+    //update balance
+    const balanceTotal = document.getElementById('balance-total');
+    const previousBalanceText = balanceTotal.innerText;
+    const previousBalanceTotal = parseFloat(previousBalanceText);
+    const newBalanceTotal = previousBalanceTotal - newWithdrawAmount;
+
+    balanceTotal.innerText = newBalanceTotal;
+    
+    // clear withdraw input
+    withdrawInput.value = ' ';
+});
---
+++ js/login.js
@@ -0,0 +1,14 @@
+document.getElementById('login-submit').addEventListener('click', function(){
+    // get user email
+    const emailField = document.getElementById('user-email');
+    const userEmail = emailField.value;
+    // get user password
+
+    const passwordField = document.getElementById('user-password');
+    const userPassword = passwordField.value;
+    
+    if(userEmail == 'sontan@gmail.com' && userPassword == 'secret'){
+        window.location.href = 'banking.html';
+    }
+});
+

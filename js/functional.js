08dd16817c369593871396399c41530765bdca15
JooJo <sjak0101@gmail.com>
2022-06-05 03:28:08 +0100

    updated some new function

--- js/functional.js
+++ js/functional.js
@@ -13,8 +13,32 @@ function updateTotal(fieldId, amount){
     const newTotal = previousTotal + amount;
     totalTag.innerText = newTotal;
 }
-
+//update balance
+function updateBalance(amount, isAdding){
+    const balanceTag = document.getElementById('balance-total');
+    const balanceText = balanceTag.innerText;
+    const previousBalance = parseFloat(balanceText);
+    let newBalance;
+    if(isAdding == true){
+         newBalance = previousBalance + amount;
+    }
+    else{
+         newBalance = previousBalance - amount;
+    }
+    
+    balanceTag.innerText = newBalance;
+    
+}
+// handle deposit
 document.getElementById('deposit-button').addEventListener('click',function(){
     const amount = getInputValue('deposit-input');
-    updateTotal('deposit-total',);
+    updateTotal('deposit-total',amount);
+    updateBalance(amount, true);
 });
+
+// handle withdraw
+document.getElementById('withdraw-button').addEventListener('click', function(){
+    const amount = getInputValue('withdraw-input');
+    updateTotal('withdraw-total', amount);
+    updateBalance(amount, false);
+})
